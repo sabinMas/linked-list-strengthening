9121fa16c572a78fb1bbfffcd6c16626a62f340f
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-05-18 15:24:53 -0700

    prepend, and remove last

--- src/Exercises.java
+++ src/Exercises.java
@@ -37,7 +37,9 @@ public class Exercises {
      * @return the new head of the linked list
      */
     public static ListNode prepend(ListNode head, int toAdd) {
-        return null;
+        ListNode newHead = new ListNode(toAdd);
+        newHead.next = head;
+        return newHead;
     }
 
     /**
@@ -53,7 +55,15 @@ public class Exercises {
      * @return the head of the list with the last element removed
      */
     public static ListNode removeLast(ListNode head) {
-        return null;
+    if (head == null || head.next == null) {
+            return null; 
+        }
+        ListNode current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        return head;
     }
 
     /**
